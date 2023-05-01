1ce9eddded1ab4cdc5be3fe2583be37663104dd7
brijesh-rakholiya <131653176+brijesh-rakholiya@users.noreply.github.com>
2023-05-01 13:25:43 +0530

    Add files via upload

---
+++ R1_21.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{   clrscr();
+    printf("my name is brijesh");
+    printf("\nmy age is 17");
+    printf("\nmy schoolm name mis");
+    getch();
+
+
+
+}
---
+++ R1_22.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{   clrscr();
+    printf("\n-----");
+    printf("\n|   |");
+    printf("\nr   |");
+    printf("\nw   |");
+    printf("\nn   |");
+    printf("\n|   |");
+    printf("\n-----");
+    getch();
+
+
+
+}
