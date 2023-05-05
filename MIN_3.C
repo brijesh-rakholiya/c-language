d09adee879b16f1043df9adf6a3440e8f91be1fb
brijesh-rakholiya <131653176+brijesh-rakholiya@users.noreply.github.com>
2023-05-05 13:49:54 +0530

    Add files via upload

---
+++ MIN_3.C
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a;
+    int b;
+    int c;
+    clrscr();
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    printf("enter the value of b:");
+    scanf("%d",&b);
+    printf("enter the value of c:");
+    scanf("%d",&c);
+    if(a<b)
+    {       if (a<c)
+	    {
+	    printf("a is min");
+	    }
+	     else
+	     {
+	     printf("c is min");
+	    }
+    }
+
+    else
+    {
+	   if(b<c)
+	   {
+	   printf("b is min");
+	   }
+	   else
+	   {
+	   printf("c is min");
+	   }
+     }
+     getch();
+}
